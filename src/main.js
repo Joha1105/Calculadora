335490c705fed7384e852dcc8f33660dc3263693
Camilo Buitrago <yoha1105@gmail.com>
2022-10-12 19:44:10 -0500

    Ejercicio de la calculadora

---
+++ src/main.js
@@ -0,0 +1,17 @@
+const botonNumeros = document.getElementsByName('number');
+
+Document.write(botonNumeros);
+
+const botonoperacion = document.getElementsByName('operacion');
+
+Document.write(botonoperacion);
+
+const botonigual = document.getElementsByName('igual')[0];
+
+document.write(botonigual);
+
+const botonborrar = document.getElementsByTagName('borrar'[0]);
+
+Document.write(botonborrar); 
+
+var resultado = document.getElementById('resultado');
